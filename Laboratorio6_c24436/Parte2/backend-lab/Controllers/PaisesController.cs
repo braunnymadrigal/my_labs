2da22dde0890afab7e415c6934c5e6b78673a858
braunnymadrigal <braunnyjmb@gmail.com>
2025-06-17 21:52:50 -0600

    Completada la Parte 1 del Laboratorio 6

---
+++ Laboratorio6_c24436/Parte2/backend-lab/Controllers/PaisesController.cs
@@ -0,0 +1,45 @@
+﻿using backend_lab.Handlers;
+using backend_lab.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        private readonly PaisesHandler _paisesHandler;
+
+        public PaisesController()
+        {
+            _paisesHandler = new PaisesHandler();
+        }
+
+        [HttpGet]
+        public List<PaisModel> Get() {
+            var paises = _paisesHandler.ObtenerPaises();
+            return paises;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> CrearPais(PaisModel pais)
+        {
+            try
+            {
+                if (pais == null)
+                {
+                    return BadRequest();
+                }
+
+                PaisesHandler paisesHandler = new PaisesHandler();
+                var resultado = paisesHandler.CrearPais(pais);
+                return new JsonResult(resultado);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error creando país");
+            }
+        }
+    }
+}
