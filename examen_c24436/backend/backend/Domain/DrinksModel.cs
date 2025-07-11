e99882feaed7275d5bf469d95f0668eeb204ec39
braunnymadrigal <braunnyjmb@gmail.com>
2025-07-11 09:29:17 -0600

    US 1: se crea el modelo de las bebidas.
    Se crea el modelo en el directorio Domain. En dicho modelo se puede almacenar el nombre de una bebida presente en la máquina expendedora, la cantidad que hay y el precio de una unidad. Este commit está asociado a la user story 1.

---
+++ examen_c24436/backend/backend/Domain/DrinksModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Domain
+{
+    public class DrinksModel
+    {
+        [Required]
+        public required string name { get; set; }
+        [Required]
+        public int quantity { get; set; }
+        [Required]
+        public int price { get; set; }
+    }
+}
