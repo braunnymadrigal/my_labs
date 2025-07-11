4fdf0883da75b3b5229541e0ff9e825cd7ef8122
braunnymadrigal <braunnyjmb@gmail.com>
2025-07-11 09:17:18 -0600

    US 1: se crea el archivo de ‘base de datos’.
    Se crea dicho archivo como una clase estática en una carpeta llamada ‘Database’, sus miembros se inicializan en un constructor estático y contienen los datos establecidos en las instrucciones. Este commit está asociado a la user story 1.

---
+++ examen_c24436/backend/backend/Database/Database.cs
@@ -0,0 +1,27 @@
+﻿namespace backend.Database
+{
+    public static class Database
+    {
+        public static Dictionary<string, (int, int)> drinks { get; set; }
+        public static Dictionary<int, int> coins { get; set; }
+
+        static Database()
+        {
+            drinks = new Dictionary<string, (int, int)>
+            {
+                { "coca cola", (10, 800) },
+                { "pepsi", (8, 750) },
+                { "fanta", (10, 950) },
+                { "sprite", (15, 975) }
+            };
+
+            coins = new Dictionary<int, int>
+            {
+                { 25, 25 },
+                { 50, 50 },
+                { 100, 30 },
+                { 500, 20 }
+            };
+        }
+    }
+}
