99ad60d57a9b913bae1f7ffebd38a398ee90e4f9
braunnymadrigal <braunnyjmb@gmail.com>
2025-07-12 15:34:50 -0600

    US 1: se crea el componente DrinksList.
    La idea es que este componente llegue a estar dentro de VendingMachine. En el componente DrinksList la idea es que se renderize la lista de bebidas, dicha lista obtenida del backend en un método del componente VendingMachine.

---
+++ examen_c24436/frontend/src/components/DrinksList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2 class="display-5 text-center mt-5">Available Drinks</h2>
+    <table class="table table-bordered text-center">
+      <thead class="table-dark">
+        <tr class="h4">
+          <th class="col-1">Name</th>
+          <th class="col-1">Price</th>
+          <th class="col-1">Stock</th>
+        </tr>
+      </thead>
+      <tbody class="table-secondary text-capitalize">
+        <tr v-for="(drink, index) in drinks" :key="index">
+          <td>{{ drink.name }}</td>
+          <td>{{ cashSymbol }}{{ drink.price }}</td>
+          <td>{{ drink.stock }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['drinks', 'cashSymbol'],
+  };
+</script>
+
+<style>
+</style>
