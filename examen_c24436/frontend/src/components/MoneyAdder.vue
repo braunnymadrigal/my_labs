8dd94bcd4f9ada66e54e9be7e2a159e8f9af469f
braunnymadrigal <braunnyjmb@gmail.com>
2025-07-12 17:48:24 -0600

    US 3: se añade el html para MoneyAdder.
    Dicho componente es el agregado para agregar el tipo y la cantidad de monedas (o billetes) que desea ingresar el usuario.

---
+++ examen_c24436/frontend/src/components/MoneyAdder.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2 class="display-5 text-center mt-5">Add Money</h2>
+    <table class="table table-bordered text-center">
+      <thead class="table-dark">
+        <tr class="h4">
+          <th class="col-1">Select Coin / Bill</th>
+          <th class="col-1">Enter Quantity</th>
+          <th class="col-1">Action</th>
+        </tr>
+      </thead>
+      <tbody class="table-secondary">
+        <tr>
+          <td>
+            <select class="form-select text-center" v-model="selectedMoney.type">
+              <option v-for="(money, index) in supportedMoney" :key="index" :value="money">
+                {{ cashSymbol }}{{ money }}
+              </option>
+            </select>
+          </td>
+          <td>
+            <input 
+              class="form-control text-center" 
+              v-model="selectedMoney.quantity" 
+              type="number"
+              min="1"
+            />
+          </td>
+          <td>
+            <button class="btn btn-light btn-outline-dark btn-md px-5" @click="checkSelectedMoney()">
+              Add
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
