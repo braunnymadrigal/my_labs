162cc18d1a70704e33cd12df47433fb579e03afa
braunnymadrigal <braunnyjmb@gmail.com>
2025-07-12 15:26:36 -0600

    US 1: se cambia el título de la página.
    Se modifica el archivo index.html para modificar el título de la página y así utilizar uno acorde. Este commit está asociado a la user story 1.

--- examen_c24436/frontend/src/App.vue
+++ examen_c24436/frontend/src/App.vue
@@ -18,10 +18,5 @@
 
   #app {
     font-family: ProtestGuerrilla;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
   }
 </style>
