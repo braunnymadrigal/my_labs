86a22d30d57990d7625dca032799386b533b7a42
braunnymadrigal <braunnyjmb@gmail.com>
2025-07-12 22:21:21 -0600

    US 4: Se agrega el endpoint.

--- examen_c24436/backend/backend/API/VendingMachineController.cs
+++ examen_c24436/backend/backend/API/VendingMachineController.cs
@@ -1,7 +1,9 @@
 ﻿using backend.Application;
+using backend.Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace backend.API
 {
@@ -14,6 +16,14 @@ namespace backend.API
         public VendingMachineController(IVendingMachine vendingMachine)
         {
             _vendingMachine = vendingMachine;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult BuyDrinks(PurchaseRequestModel purchaseRequest)
+        {
+            var purchaseResponse = _vendingMachine.buyDrinks(purchaseRequest);
+            return Ok(purchaseResponse);
         }
     }
 }
